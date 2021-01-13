6f207762f771a64fe616d678c426994eb9e5123a
navneet <navneetmaharishi@gmail.com>
2021-01-13 22:54:36 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+import Error from './Error';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Functional Component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
+//     return <div>Hi There</div>
+// }
+
+// Class Component
+
+class App extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = { lat: null, errorMsg: '' }
+
+    //     window.navigator.geolocation.getCurrentPosition(
+    //         position => {
+    //             this.setState({ lat: position.coords.latitude });
+    //         },
+    //         err => {
+    //             this.setState({ errorMsg: err.message });
+    //         }
+    //     );
+    // };
+
+    state = {
+        lat: null,
+        errorMsg: ''
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMsg: err.message })
+        );
+    }
+
+    renderContent() {
+        if (this.state.errorMsg && !this.state.lat) {
+            return <Error msg={this.state.errorMsg} />
+        };
+
+        if (!this.state.errorMsg && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        };
+        return <Loader msg='Please accept location request' />;
+    };
+
+    render() {
+        return (
+            <div className="warper">
+                {this.renderContent()}
+            </div>
+        );
+
+        // if (this.state.errorMsg && !this.state.lat) {
+        //     return <div> Error: {this.state.errorMsg}</div >;
+        // };
+
+        // if (!this.state.errorMsg && this.state.lat) {
+        //     return <SeasonDisplay lat={this.state.lat} />;
+        // };
+
+        // return <Loader msg='Please accept location request' />;
+    };
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
